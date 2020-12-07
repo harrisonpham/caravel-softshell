53a8cdd8fd810680ea0aafbf71f978bda2640dc6
Harrison Pham <harrison@harrisonpham.com>
2020-12-07 02:38:54 +0000

    Softshell user project and wrapper config updates.

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -4,19 +4,53 @@ set ::env(DESIGN_NAME) user_proj_example
 
 set ::env(VERILOG_FILES) "\
 	$script_dir/../../verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/user_proj_example.v"
+	$script_dir/../../verilog/rtl/user_proj_example.v \
+	$script_dir/../../softshell/rtl/softshell_top.v \
+	$script_dir/../../softshell/rtl/rv_core.v \
+	$script_dir/../../softshell/third_party/verilog-wishbone/rtl/wb_arbiter_5.v \
+	$script_dir/../../softshell/third_party/verilog-wishbone/rtl/arbiter.v \
+	$script_dir/../../softshell/third_party/verilog-wishbone/rtl/priority_encoder.v \
+	$script_dir/../../softshell/third_party/verilog-wishbone/rtl/wb_mux_3.v \
+	$script_dir/../../softshell/third_party/verilog-wishbone/rtl/wb_mux_4.v \
+	$script_dir/../../softshell/third_party/picorv32_wb/mem_ff_wb.v \
+	$script_dir/../../softshell/third_party/picorv32_wb/simpleuart.v \
+	$script_dir/../../softshell/third_party/picorv32_wb/spimemio.v \
+	$script_dir/../../softshell/third_party/picorv32_wb/gpio32_wb.v \
+	$script_dir/../../softshell/third_party/picorv32_wb/picorv32.v"
 
-set ::env(CLOCK_PORT) ""
-set ::env(CLOCK_NET) "counter.clk"
-set ::env(CLOCK_PERIOD) "10"
+#set ::env(VERILOG_INCLUDE_DIRS) "\
+#	$script_dir/../../softshell"
+
+set ::env(CLOCK_PORT) "wb_clk_i"
+#set ::env(CLOCK_NET) "softshell.wb_clk_i"
+set ::env(CLOCK_PERIOD) "50"
+set ::env(SYNTH_STRATEGY) 2
+set ::env(SYNTH_MAX_FANOUT) 4
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 600 600"
+set ::env(DIE_AREA) "0 0 2500 3000"
 set ::env(DESIGN_IS_CORE) 0
+
+set ::env(FP_PDN_CORE_RING) 0
+
+#set ::env(GLB_RT_ALLOW_CONGESTION) 1
+#set ::env(GLB_RT_MINLAYER) 2
+set ::env(GLB_RT_MAXLAYER) 5
+
+#set ::env(PL_TARGET_DENSITY) 0.30
+
+set ::env(PL_TARGET_DENSITY) 0.52
+set ::env(PL_TARGET_DENSITY_CELLS) 0.38
+set ::env(PL_OPENPHYSYN_OPTIMIZATIONS) 1
+set ::env(CELL_PAD) 4
+
+set ::env(GLB_RT_ADJUSTMENT) 0
+set ::env(GLB_RT_TILES) 14
+
+set ::env(DIODE_INSERTION_STRATEGY) 1
+
+set ::env(ROUTING_CORES) 11
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 # set ::env(FP_CONTEXT_DEF) $script_dir/../user_project_wrapper/runs/user_project_wrapper/tmp/floorplan/ioPlacer.def.macro_placement.def
 # set ::env(FP_CONTEXT_LEF) $script_dir/../user_project_wrapper/runs/user_project_wrapper/tmp/merged_unpadded.lef
-
-set ::env(PL_BASIC_PLACEMENT) 1
-set ::env(PL_TARGET_DENSITY) 0.15
