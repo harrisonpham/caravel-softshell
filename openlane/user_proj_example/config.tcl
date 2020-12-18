72e99b8dccc8fb6077284134db802742dbaf96a8
Harrison Pham <harrison@harrisonpham.com>
2020-12-18 22:53:37 +0000

    Good macro configs from 3cpu-512w-nobuff-wrapped.
    From commit 4e60cd8416751ad0bf38814006a930c58a6f0468

--- openlane/user_proj_example/config.tcl
+++ openlane/user_proj_example/config.tcl
@@ -1,41 +1,57 @@
-# SPDX-FileCopyrightText: 2020 Efabless Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# SPDX-License-Identifier: Apache-2.0
-
 set script_dir [file dirname [file normalize [info script]]]
 
 set ::env(DESIGN_NAME) user_proj_example
 
 set ::env(VERILOG_FILES) "\
 	$script_dir/../../verilog/rtl/defines.v \
-	$script_dir/../../verilog/rtl/user_proj_example.v"
+	$script_dir/../../verilog/rtl/user_proj_example.v \
+	$script_dir/../../verilog/rtl/softshell/rtl/softshell_top.v \
+	$script_dir/../../verilog/rtl/softshell/rtl/rv_core.v \
+	$script_dir/../../verilog/rtl/softshell/rtl/pinmux.v \
+	$script_dir/../../verilog/rtl/softshell/rtl/pcpi_flexio.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/verilog-wishbone/rtl/wb_arbiter_3.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/verilog-wishbone/rtl/wb_arbiter_4.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/verilog-wishbone/rtl/wb_arbiter_5.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/verilog-wishbone/rtl/arbiter.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/verilog-wishbone/rtl/priority_encoder.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/verilog-wishbone/rtl/wb_mux_3.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/verilog-wishbone/rtl/wb_mux_5.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/picorv32_wb/mem_ff_wb.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/picorv32_wb/simpleuart.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/picorv32_wb/spimemio.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/picorv32_wb/gpio32_wb.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/picorv32_wb/picorv32.v \
+	$script_dir/../../verilog/rtl/softshell/third_party/wb2axip/rtl/afifo.v"
 
-set ::env(CLOCK_PORT) ""
-set ::env(CLOCK_NET) "counter.clk"
-set ::env(CLOCK_PERIOD) "10"
+#set ::env(VERILOG_INCLUDE_DIRS) "\
+#	$script_dir/../../softshell"
+
+# For the manually instantiated buffers in softshell_top.
+# set ::env(SYNTH_READ_BLACKBOX_LIB) 1
+
+set ::env(CLOCK_PORT) "wb_clk_i"
+#set ::env(CLOCK_NET) "softshell.wb_clk_i"
+set ::env(CLOCK_PERIOD) "20"
 
 set ::env(FP_SIZING) absolute
-set ::env(DIE_AREA) "0 0 600 600"
+set ::env(DIE_AREA) "0 0 2500 3100"
 set ::env(DESIGN_IS_CORE) 0
+set ::env(FP_PDN_CORE_RING) 0
 
-set ::env(VDD_NETS) [list {vccd1} {vccd2} {vdda1} {vdda2}]
-set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
+#set ::env(GLB_RT_ALLOW_CONGESTION) 1
+set ::env(GLB_RT_MAXLAYER) 5
+
+# 0.4 results in shorts, 0.35 - 0.3 sometimes results in metal loops due to routing.
+set ::env(PL_TARGET_DENSITY) 0.38
+
+# Don't use met5.
+set ::env(GLB_RT_OBS) "met5 0 0 2500 3100"
+
+# Diodes inserted using interactive.tcl.
+set ::env(DIODE_INSERTION_STRATEGY) 0
+
+set ::env(ROUTING_CORES) 6
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
-
-set ::env(PL_BASIC_PLACEMENT) 1
-set ::env(PL_TARGET_DENSITY) 0.15
-
-# If you're going to use multiple power domains, then keep this disabled.
-set ::env(RUN_CVC) 0
+# set ::env(FP_CONTEXT_DEF) $script_dir/../user_project_wrapper/runs/user_project_wrapper/tmp/floorplan/ioPlacer.def.macro_placement.def
+# set ::env(FP_CONTEXT_LEF) $script_dir/../user_project_wrapper/runs/user_project_wrapper/tmp/merged_unpadded.lef
---
+++ openlane/user_project_wrapper/interactive.tcl
@@ -0,0 +1,41 @@
+package require openlane
+set script_dir [file dirname [file normalize [info script]]]
+
+prep -design $script_dir -tag user_project_wrapper -overwrite
+set save_path $script_dir/../..
+
+verilog_elaborate
+
+init_floorplan
+
+place_io_ol
+
+add_macro_placement mprj 200 320 N
+
+manual_macro_placement f
+
+exec -ignorestderr openroad -exit $script_dir/gen_pdn.tcl
+set_def $::env(pdn_tmp_file_tag).def
+
+global_routing_or
+add_route_obs
+detailed_routing
+
+write_powered_verilog -power vccd1 -ground vssd1
+set_netlist $::env(lvs_result_file_tag).powered.v
+
+run_magic
+run_magic_spice_export
+
+save_views       -lef_path $::env(magic_result_file_tag).lef \
+                 -def_path $::env(tritonRoute_result_file_tag).def \
+                 -gds_path $::env(magic_result_file_tag).gds \
+                 -mag_path $::env(magic_result_file_tag).mag \
+                 -save_path $save_path \
+                 -tag $::env(RUN_TAG)
+
+run_magic_drc
+
+run_lvs; # requires run_magic_spice_export
+
+run_antenna_check
